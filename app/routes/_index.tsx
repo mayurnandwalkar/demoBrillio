9fff5b95609c66440b961cdc4de99a0212c029ee
mayurnandwalkar <mayur@owowtalents.comgit>
2024-10-03 23:37:45 +0530

    code updated

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import Header from '~/Components/Header'
+import Footer from '~/Components/Footer'
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +13,7 @@ export default function Index() {
   return (
     <>
     <Header/>
+    <Footer/>
     </>
   );
 }
